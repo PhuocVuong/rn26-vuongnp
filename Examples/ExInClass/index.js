af5e96a98cd48262a8a5117ba44c3549382402ad
PhuocVuong <npv9698@gmail.com>
2022-10-06 16:15:07 +0700

    HomeWork3-update1

---
+++ Examples/ExInClass/index.js
@@ -0,0 +1,15 @@
+
+let hihi = 19;
+      const phone = 799963969;
+      let array1 = [
+        {
+          Title: "GiaDinhNhaTao",
+          member: "Four",
+          Color: "Green",
+        },
+      ];
+      console.log("number", hihi);
+      console.log("string", "abc123");
+      console.log (phone);
+      console.log (array1);
+      console.log ( array1.Title );
